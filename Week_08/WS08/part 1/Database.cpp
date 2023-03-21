3bf75d797533d94395be6b804e9a7a8e7c87ac3b
AryanK1511 <aryankhurana1511@gmail.com>
2023-03-21 09:53:48 -0400

    Added WS08 starter code

---
+++ Week_08/WS08/part 1/Database.cpp
@@ -0,0 +1,16 @@
+/*
+*****************************************************************************
+                              Database.cpp
+Full Name  : Aryan Khurana
+Student ID#: 145282216
+Email      : akhurana22@myseneca.ca
+Date of completion    : 21 March 2023
+I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+*****************************************************************************
+*/
+
+#include "Database.h"
+
+namespace sdds {
+
+}
---
+++ Week_08/WS08/part 1/Database.h
@@ -0,0 +1,21 @@
+/*
+*****************************************************************************
+                              Database.h
+Full Name  : Aryan Khurana
+Student ID#: 145282216
+Email      : akhurana22@myseneca.ca
+Date of completion    : 21 March 2023
+I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+*****************************************************************************
+*/
+
+#ifndef SDDS_DATABASE_H
+#define SDDS_DATABASE_H
+
+namespace sdds {
+    class Database {
+
+    };
+}
+
+#endif // SDDS_DATABASE_H
